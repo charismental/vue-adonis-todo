47e977bcc3600905468ded8ed8c7289f84da1cd7
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2018-10-10 11:35:50 -0700

    Registration

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 import { sync } from 'vuex-router-sync';
 import Vuetify from 'vuetify';
 import Vue from 'vue';
+import 'vuetify/dist/vuetify.min.css';
 import App from './App.vue';
 import router from './router';
 import store from './store/index';
